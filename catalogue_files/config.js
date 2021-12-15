852cdb578b43f86f2b13d41739f09e3960fa247e
michelevdokimov <michel.evdokimov@gmail.com>
2021-12-15 15:47:32 +0300

    Add files via upload

---
+++ catalogue_files/config.js
@@ -0,0 +1,5 @@
+
+window[window["TiktokAnalyticsObject"]].instance("BV2F4A2D4EIVEV2IP3R0").setAdvancedMatchingAvailableProperties({"email":false,"phone_number":false});
+window[window["TiktokAnalyticsObject"]].instance("BV2F4A2D4EIVEV2IP3R0").setPixelInfo && window[window["TiktokAnalyticsObject"]].instance("BV2F4A2D4EIVEV2IP3R0").setPixelInfo({status: 0, name: "Showroom hyundai", advertiserID: "6864922173102882821", setupMode: 0, partner: "", is_onsite: false});
+window[window["TiktokAnalyticsObject"]].setPCMDomain && window[window["TiktokAnalyticsObject"]].setPCMDomain("hyundai.ru");
+window[window["TiktokAnalyticsObject"]].setPCMConfig && window[window["TiktokAnalyticsObject"]].setPCMConfig([]);
